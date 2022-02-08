7fd889fbd2c8b98747ff1bb92ae9784621b7ff8d
LeeJin0527 <ijin17463@gmail.com>
2022-02-08 21:23:41 +0900

    추상 클래스와 인터페이스 실습 상태의 결과값이 null 이 나오는 이슈 발생

--- src/com/company/프로젝트라이언/Week1/Week1HW/Main.java
+++ src/com/company/프로젝트라이언/Week1/Week1HW/Main.java
@@ -3,7 +3,7 @@ package com.company.프로젝트라이언.Week1.Week1HW;
 public class Main {
     public static void main(String[] args) {
         Student student = new Student("이진", 1);
-//        student.speak();
+        student.speak();
 
         Lecturer lecturer = new Lecturer("이호맹", 2);
         lecturer.speak();
